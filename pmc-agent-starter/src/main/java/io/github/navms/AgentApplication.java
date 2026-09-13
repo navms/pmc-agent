0b2d63bf118da6e18b7751db507f232fd4354386
hein.hpq <hein.hpq@gmail.com>
2026-09-13 22:02:44 +0800

    opt: AgentScope

--- pmc-agent-starter/src/main/java/io/github/navms/AgentApplication.java
+++ pmc-agent-starter/src/main/java/io/github/navms/AgentApplication.java
@@ -8,14 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author navms
  */
-@SpringBootApplication(scanBasePackages = {
-        "io.github.navms.application",
-        "io.github.navms.agent",
-        "io.github.navms.domain.chat",
-        "io.github.navms.infrastructure",
-        "io.github.navms.tool",
-        "io.github.navms.web"
-})
+@SpringBootApplication
 public class AgentApplication {
 
     public static void main(String[] args) {
